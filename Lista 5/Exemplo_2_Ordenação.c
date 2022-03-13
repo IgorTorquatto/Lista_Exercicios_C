0f13167bc2f74b23310a464372e9d23ff36933ab
IgorTorquatto <igor.torquato.20@gmail.com>
2022-03-13 12:08:05 -0300

    Novos arquivos

---
+++ Lista 5/Exemplo_2_Ordenação.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y,aux,*v,z;
+    printf("Digite o tamanho do vetor:");
+    scanf("%d",&z);
+    
+    v=(int *)malloc(z*sizeof(int));
+    
+    for(int i=0;i<z;i++){
+        printf("Digite o valor para a posição %d do vetor: ",i+1);
+        scanf("%d",&v[i]);
+    }
+    
+    printf("\nOrdenando o vetor:\n");
+    
+    for(x=0;x<z;x++){
+        for(y=x+1;y<z;y++){
+            if(v[y]<v[x]){
+                aux=v[x];
+                v[x]=v[y];
+                v[y]=aux;
+            }
+        }
+    }
+    
+    for(int i=0;i<z;i++){
+        printf("(%d)",v[i]);
+    }
+   
+    return 0;
+}
+
